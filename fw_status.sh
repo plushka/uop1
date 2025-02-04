5a9eaef90d40c5d9d1c98392d6b13481105cd29d
plushka <110601305+plushka@users.noreply.github.com>
2025-02-04 17:36:16 +0100

    Create fw_status.sh

---
+++ fw_status.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+LOGFILE="/var/log/firewall.log"
+STATEFILE="/var/lib/fw_drop_stats.db"
+TMP="/tmp/fw_drop_stats.tmp"
+
+[ -f $STATEFILE ] || touch $STATEFILE
+
+declare -A STATS
+while read line; do
+    IP=$(echo "$line" | cut -d' ' -f1)
+    COUNT=$(echo "$line" | cut -d' ' -f2)
+    STATS[$IP]=$COUNT
+done < $STATEFILE
+
+tail -n 1000 $LOGFILE | grep "FW_DROP_55to50" | while read l; do
+    # V logu to bývá např. "... SRC=192.168.55.X DST=192.168.50.Y ..."
+    SRC=$(echo "$l" | sed -n 's/.*SRC=\([^ ]*\).*/\1/p')
+    if [ -n "$SRC" ]; then
+        # Zvýšíme čítač
+        if [ -z "${STATS[$SRC]}" ]; then
+            STATS[$SRC]=0
+        fi
+        STATS[$SRC]=$(( ${STATS[$SRC]} + 1 ))
+    fi
+done
+
+> $STATEFILE
+for IP in "${!STATS[@]}"; do
+    echo "$IP ${STATS[$IP]}" >> $STATEFILE
+done
